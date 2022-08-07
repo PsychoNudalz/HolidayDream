3247e0a26482910cfa0cb16371d0acbfd2ab2fe8
Psycho_Nudalz <anson.aw0618@gmail.com>
2022-08-07 14:44:46 +0100

    Modified living room, improved shader

--- Assets/Scripts/MaterialPulse.cs
+++ Assets/Scripts/MaterialPulse.cs
@@ -11,16 +11,34 @@ public class MaterialPulse : MonoBehaviour
     [SerializeField]
     private Material[] environmentMaterials;
 
+    private Vector3 originalPos = new Vector3();
+
     private void Awake()
     {
         mainCamera = Camera.main;
+        originalPos = mainCamera.transform.position;
     }
 
     private void FixedUpdate()
     {
+        SetMaterialPos(mainCamera.transform.position);
+    }
+
+    private void SetMaterialPos(Vector3 pos)
+    {
         foreach (Material environmentMaterial in environmentMaterials)
         {
-            environmentMaterial.SetVector("_PlayerPos", mainCamera.transform.position);
+            environmentMaterial.SetVector("_PlayerPos", pos);
         }
+    }
+
+    private void OnApplicationQuit()
+    {
+        SetMaterialPos(originalPos);
+    }
+
+    private void OnDestroy()
+    {
+        SetMaterialPos(originalPos);
     }
 }
